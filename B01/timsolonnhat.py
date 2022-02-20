5a0bd94817edf12d0425ca33f67b18475ee3783a
AdTekDev <nguyenngtu777@gmail.com>
2022-02-20 09:28:22 +0700

    Add files via upload

---
+++ B01/timsolonnhat.py
@@ -0,0 +1,16 @@
+a = input("Nhap so A: ")
+b = input("Nhap so B: ")
+c = input("Nhap so C: ")
+
+
+if (a > b):
+    if (a > c):
+        print("A")
+    else:
+        print("C")
+else:
+    if (b > c):
+        print("B")
+    else:
+        print("C")
+
