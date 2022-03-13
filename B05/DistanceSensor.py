ddf86d96911583dcb43ff42ea91ea109e90068c0
AdTekDev <nguyenngtu777@gmail.com>
2022-03-13 10:27:27 +0700

    Add files via upload

---
+++ B05/DistanceSensor.py
@@ -0,0 +1,11 @@
+
+# https://gpiozero.readthedocs.io/en/stable/recipes.html#distance-sensor
+from gpiozero import DistanceSensor
+from time import sleep
+
+sensor = DistanceSensor(23, 24)
+
+while True:
+    print('Distance to nearest object is', sensor.distance, 'm')
+    sleep(1)
+
